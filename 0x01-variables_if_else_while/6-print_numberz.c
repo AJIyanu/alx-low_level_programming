ae33d274bc67088244df146e19fdc23e8cd8b000
AJIyanu <aderemijoseph@live.com>
2022-03-12 06:26:16 +0100

    sync

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* We have Just called in the standard input lib */
+
+
+/* Now we are in the main code
+ * what this code does it print every digits in base ten
+ * I am only allowed to use a putchar()
+ * All my code should be main
+ * We are also going to return void
+ */
+
+int main()
+{
+
+	char a = 0;
+	for (char = 0; a <= 9; a++;)
+		putchar(a);
+	
+
+	putchar("\n");
+	
+	return(0);
+
+
+}
