3e2be1a684514f2d2b2991269010946e43ad6c5a
AJIyanu <aderemijoseph@live.com>
2022-03-12 20:58:54 +0100

    code sucess but had to conform to betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 
-/* This program is going to generate a random number and store 
- * in a n, the program then checks if the number stored is positive or negagive.
+/* This program is going to generate a random number and store
+ * in a n, the program then checks if the number stored is p
  * it then give us the result
  */
 
 int main(void)
 {
 
+	/* This is code that generates and stored the random number */
 	int n;
 
 	srand(time(0));
@@ -17,12 +18,14 @@ int main(void)
 
 	/* the above code generates the random number, now to tbe check */
 
-	if (n > 0)
-		printf ("%d is positive\n", n);
-	else if (n == 0)
-		printf ("%d is zero\n", n);
+	if(n > 0)
+		printf("%d is positive\n", n);
+	/* This checks if the number is positive */
+	else if(n == 0)
+		printf("%d is zero\n", n);
+	/* This checks if the number is zero */
 	else
-		printf ("%d is negative\n", n);
+		printf("%d is negative\n", n);
 
 	return (0);
 
