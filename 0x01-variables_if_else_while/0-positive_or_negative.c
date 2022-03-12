ae33b71eb046ddff0de4c8b6aa3153bafe888a7b
AJIyanu <aderemijoseph@live.com>
2022-03-12 20:46:17 +0100

    finally wrote my first program correctly

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* Bety style doc for function goes there */
+/* This program is going to generate a random number and store 
+ * in a n, the program then checks if the number stored is positive or negagive.
+ * it then give us the result
+ */
 
 int main(void)
 {
@@ -11,7 +14,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	/* your code goes there */
+	/* the above code generates the random number, now to tbe check */
+
+	if (n > 0)
+		printf ("%d is positive\n", n);
+	else if (n == 0)
+		printf ("%d is zero\n", n);
+	else
+		printf ("%d is negative\n", n);
 
 	return (0);
 
