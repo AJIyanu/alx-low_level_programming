638386496f94460926b82ca9c9c2a67cf0a6ddec
AJIyanu <aderemijoseph@live.com>
2022-03-13 18:19:10 +0100

    betty should orimt error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
  * @a: used to reduce to last digit
  *
  * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -18,20 +19,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* I dont know the above code buh this is mine */
-
+	
 	a = n;
 	while (a > 10)
-	a = a / 10;
+	a = a % 10;
 
 	if (a > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, a)
+	printf("Last digit of %d is %d and is greater than 5\n", n, a);
 
 	else if (a < 6 && a != 0)
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 
 	else
-	printf("Last digit of %d is %d and is 0\n", n, a)
+	printf("Last digit of %d is %d and is 0\n", n, a);
 
 
 
