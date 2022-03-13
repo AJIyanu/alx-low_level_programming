ce00cc34522ff9398e39b47024b513af7f20b128
AJIyanu <aderemijoseph@live.com>
2022-03-13 23:22:52 +0100

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* This is a code to print alphabets
+ * from a to z in small letters from
+ * the ascii value. we are going to
+ * run an loop
+ */
+
+
+/**
+ * main - this is where the code goes
+ * Rerurn: void
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return(0);
+
+}
