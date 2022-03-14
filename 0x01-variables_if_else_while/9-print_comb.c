f3952ab3e6ec7b32df3ba1f164d04923081c180a
AJIyanu <aderemijoseph@live.com>
2022-03-14 10:11:32 +0100

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
+/*
  * This code is going to print from
  * 0 - 9 separated by a comma and whitespace
  */
- 
+
 /**
  * main - This where all codes go
  * Return: void
  */
- 
- int main(void)
- {
-	 int za;
-	 
-	 for (za = '0'; za <= '9'; za++)
-	 {	putchar(za);
-		putchar(',');
-		putchar(' ');
-	 }
-		putchar('\n');
-		return (0);
-	 
- }
+
+int main(void)
+{
+	int za;
+	for (za = '0'; za <= '9'; za++)
+	{	putchar(za);
+		
+		if (za != '9')
+		{	putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0); 
+}
