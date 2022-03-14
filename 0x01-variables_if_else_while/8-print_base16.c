d989dc36087f48a760f233bf9af620a1e677a681
AJIyanu <aderemijoseph@live.com>
2022-03-14 10:03:44 +0100

    betty corrected

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
+/*
  * This code is going to print
  * the digits and letters of hexadecimal
  */
- 
+
 /**
  * main - This where all codes go
  * Return: void
  */
- 
- int main(void)
- {
+
+int main(void)
+{
 	char hexa;
-	 
+
 	for (hexa = '0'; hexa <= '9'; hexa++)
 		putchar(hexa);
-	
+
 	for (hexa = 'a'; hexa <= 'f'; hexa++)
 		putchar(hexa);
-		
+
 	putchar('\n');
 	return (0);
-	 
- }
+}
