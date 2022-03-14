e58099dedb75470ef8a8e61b35ba2363a2d565c3
AJIyanu <aderemijoseph@live.com>
2022-03-14 09:51:50 +0100

    betty oooo

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,24 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
+/*fc
  * This code is going to print from
  * z - a lower case
  */
- 
+
 /**
  * main - This where all codes go
  * Return: void
  */
- 
- int main(void)
- {
-	 char za;
-	 
-	 for (za = 'z'; za >= 'a'; za--)
-			putchar(za);
-		
+
+int main(void)
+{
+	char za;
+	for (za = 'z'; za >= 'a'; za--)
+		putchar(za);
+
 		putchar('\n');
 		return (0);
-	 
  }
