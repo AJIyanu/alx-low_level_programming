5736147ce4bb2dd0b4f532bafd323bd46d72b5b6
AJIyanu <aderemijoseph@live.com>
2022-03-15 05:39:25 +0100

    managed out some ojoro

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * This is going to print the comnination of
+ * two numbers so that none of them is repeated.
+ * okay lets go
+ */
+
+/**
+ * main - this is thd main
+ * Return: void
+ */
+
+int main(void)
+{
+
+	int d;
+	int g;
+	int i;
+	int t;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+
+		for (g = '0'; g <= '9'; g++)
+		{
+			for (i = '0'; i <= '9'; i++)
+			{
+				for (t ='0'; t <= '9'; t++)
+				{
+					if ((d + g) < (i + t))
+					{
+						putchar(d);
+						putchar(g);
+						putchar(' ');
+						putchar(i);
+						putchar(t);
+
+						if (d == '9' && g == '8')
+							break;
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
