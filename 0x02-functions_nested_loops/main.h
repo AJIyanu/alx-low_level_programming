cf0df19563e19663d20fcea7c39961bb7b050161
AJIyanu <aderemijoseph@live.com>
2022-03-16 04:02:59 +0100

    added my function to header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,14 @@ int _putchar(char c)
 }
 
 
+void print_alphabet(void)
+{
+        int i;
+        for (i = 'a'; i <= 'z'; i++)
+                _putchar(i);
+
+        _putchar('\n');
+        return;
+
 
 #endif
