8cf816fca22bdf8b0abfeffb5e132e278abcbae3
AJIyanu <aderemijoseph@live.com>
2022-03-16 12:57:29 +0100

    added header without print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,16 +16,6 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-void print_alphabet(void)
-{
-        int i;
-        for (i = 'a'; i <= 'z'; i++)
-                _putchar(i);
-
-        _putchar('\n');
-        return;
-}
-
 
 
 #endif
