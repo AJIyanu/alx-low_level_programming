6d40b5c57ab23aae346e73bc895b2add40ca9e40
AJIyanu <aderemijoseph@live.com>
2022-03-17 11:29:15 +0100

    stopped using problem function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,19 +15,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
 	}
 
 	else if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return (0);
 	}
 
 	else
 	{
-		_putchar('-');
+		putchar('-');
 		return (-1);
 	}
 }
