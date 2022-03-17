07334a8685677a4c8b410371a51655e3ab1181c6
AJIyanu <aderemijoseph@live.com>
2022-03-17 10:17:44 +0100

    added all prototypes to header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -64,3 +64,17 @@ int print_last_digit(int c)
 
 
 int _putchar(int);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
