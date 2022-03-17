f0d66a291f212e4d378c016a202e3c90b722e4c1
AJIyanu <aderemijoseph@live.com>
2022-03-17 19:55:29 +0100

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * we are going to print the ten
+ * digits and a new line
+ */
+
+/**
+ * print_numbers - code
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c;
+	
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return;
+}
