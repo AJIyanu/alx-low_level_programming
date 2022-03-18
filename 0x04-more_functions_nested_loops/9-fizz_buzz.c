f9f3ef091646c527bca6172fcf68b8fc6b76dc96
AJIyanu <aderemijoseph@live.com>
2022-03-18 15:02:30 +0100

    bad ass at debugging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,14 +19,16 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%d ", num);
+		if (num != 100)
+			printf (" ");
 	}
 	printf("\n");
-	Return (0);
+	return (0);
 }
