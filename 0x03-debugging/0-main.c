9d49eb902f0020afcd4e32bf5e906d58d2fbf367
AJIyanu <aderemijoseph@live.com>
2022-03-18 19:34:29 +0100

    betty please

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,13 @@
 #include "main.h"
+/*
+ * This function testes positivity
+ * and negativity
+ */
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if inte
+ * Return: 0
+ */
 
 int main(void)
 {
