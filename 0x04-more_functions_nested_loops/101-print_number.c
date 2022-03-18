8fae41441f0686c957afaa2c455710e82c79dd9c
AJIyanu <aderemijoseph@live.com>
2022-03-18 16:42:07 +0100

    does betty conform??

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,9 +11,17 @@
  * Return: void
  */
 
+
 void print_number(int n)
 {
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+
 	if (n / 10)
 		print_number(n / 10);
-	putchar('0' + n % 10);
+
+	putchar(n % 10 + '0');
 }
