efdb3872c5939ad08be761a3a0665a5f949f396e
AJIyanu <aderemijoseph@live.com>
2022-03-18 07:36:15 -0700

    I hope i have built a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+ * This function is for printing
+ * triangle
+ */
+
+/**
+ * print_triangle - main code here
+ * @size: height of the triangle
+ * Return: how about a void
+ */
+
+void print_triangle(int size)
+{
+	int row;
+	int column;
+	int space;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			space = size - row;
+			
+			for (column = 0; column < space; column++)
+				putchar(' ');
+			for (column = 0; column < row; column++)
+				putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+		putcahr('\n');
+}
+			
