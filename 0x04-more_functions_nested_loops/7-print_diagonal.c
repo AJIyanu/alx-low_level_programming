c83cf5cb5b2a8ba42b42ddfaf0f0c6ab638aac76
AJIyanu <aderemijoseph@live.com>
2022-03-18 04:59:08 -0700

    print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * This function is going to print 
+ * diagonal line with \ and that 
+ * will be all
+ */
+
+/**
+ * print_diagonal - code here
+ * @n: number of diagonnals to print
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int l;
+	int space;
+
+	for (l = 0; l < n; l++)
+	{
+		for (space = 0; space < l; space++)
+			putchar(' ');
+		putchar(92);
+		putchar('\n');
+	}
+}
