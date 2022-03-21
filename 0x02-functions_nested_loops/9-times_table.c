d32b8dd18197c200cf2a27d224d1329094787245
AJIyanu <aderemijoseph@live.com>
2022-03-21 07:20:22 +0100

    multiplication table ready-betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 			if (j == 0)
 				printf("%d", i * j);
 			else if (i * j > 9)
-				printf(", %d", i *j);
+				printf(", %d", i * j);
 			else
 				printf(",  %d", i * j);
 		}
