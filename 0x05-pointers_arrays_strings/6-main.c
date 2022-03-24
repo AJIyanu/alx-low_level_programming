715cdd49867f6a11ed7feacd126408c30cb2e70b
AJIyanu <aderemijoseph@live.com>
2022-03-24 11:15:32 +0100

    bug removed

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "me i am testing string null character in all places\0 alright";
+    puts2(str);
+    return (0);
+}
