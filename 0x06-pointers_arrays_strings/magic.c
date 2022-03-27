2ead67d0a47e8a8e25f2665d88c342467a354d2e
AJIyanu <aderemijoseph@live.com>
2022-03-27 14:27:01 -0700

    further learning on memory allocation in ubuntu

--- 0x06-pointers_arrays_strings/magic.c
+++ 0x06-pointers_arrays_strings/magic.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -16,8 +16,13 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	*(p + 5) = 98;
+//  *(p + 5) = 98;
+  *(p + 4) = 99;
+  *(p + 3) = 100;
+  *(p + 2) = 101;
+  *(p + 1) = 102;
+  *p = 103;
   /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  printf("%d\n%d\n%d\n%d\n", *p, *a, a[1], a[2]);
+  return (0);
 }
