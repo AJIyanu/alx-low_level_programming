9aed2c6756bf9912977dfd47a8e3c7b372e2fd14
AJIyanu <aderemijoseph@live.com>
2022-03-27 03:32:25 +0100

    times table of n numbers

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - prints times table of a given num
+ * @n: size of the table
+ * Return: nothing is returned
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n < 16 && n >= 0)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				if (i * j > 99 && j != 0)
+					printf(" ");
+				else if (i * j > 9 && j != 0)
+					printf("  ");
+				else if (j != 0)
+					printf("   ");
+				printf("%d", i * j);
+			}
+			printf("\n");
+		}
+	}
+}
