d89b2e1c325c6c2ddfc7bff9f9e71bc1d79f94e2
AJIyanu <aderemijoseph@live.com>
2022-03-29 16:36:46 +0100

    first check.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - finds the first occurence of any of the word
+ * @s: string to check
+ * @accept: string to check with
+ *
+ * Return: pointer to the string.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+	}
+	return (&s[i]);
+}
