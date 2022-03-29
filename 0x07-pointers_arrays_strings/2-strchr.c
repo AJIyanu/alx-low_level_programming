9e6e79d037a84937df1de624b2e8f4027efeb32c
AJIyanu <aderemijoseph@live.com>
2022-03-29 04:43:31 +0100

    send your location

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strchr - locates a string
+ * @s: string base address
+ * @c: character to be located
+ *
+ * Return: returns address to the string and NULL if not present
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+	}
+	return (&s[i]);
+}
+
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is where i learnt multidimensional array
+and working with pointers leading to another pointer
