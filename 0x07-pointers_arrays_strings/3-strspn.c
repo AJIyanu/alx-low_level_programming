22f382b23cbaa4f7fb42c9d430c9d5e450af0880
AJIyanu <aderemijoseph@live.com>
2022-03-28 21:53:32 -0700

    prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @s: string to count
+ * @accept: string to chexk with
+ *
+ * Return: nothing parsie
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c = 0;
+
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+				c++;
+		}
+	}
+	return (c);
+}
+
