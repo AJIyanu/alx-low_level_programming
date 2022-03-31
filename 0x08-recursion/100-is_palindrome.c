e2a53587a575a8933d07b814f482ef248bb18358
AJIyanu <aderemijoseph@live.com>
2022-03-31 09:53:34 +0100

    They say we only use a fraction of our brain's true potential. Now that is when we are awake. When we are asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * _strlen_recursion - finds the length of a string recursively
  * @s: address to the base of string array
@@ -13,6 +14,25 @@ int _strlen_recursion(char *s)
 }
 
 /**
+ * is_palindrome2 - checks if numbers repeat from behind
+ * @s: string to compare
+ * @l: length of string to compare
+ * Return: 1 on success
+ */
+
+int is_palindrome2(char *s, int l)
+{
+	int r = 0;
+	if (l < 0)
+		return (1);
+	else if (*s == s[l])
+		r = is_palindrome2(s + 1, l - 2);
+	return (r);
+}
+	
+
+
+/**
  * is_palindrome - checks if numbers repeat from behind
  * @s: string to compare
  * Return: 1 on success
@@ -21,6 +41,19 @@ int _strlen_recursion(char *s)
 int is_palindrome(char *s)
 {
 	int l;
+	int r;
+
+	l = _strlen_recursion(s);
+	if (l == 0)
+		return (1);
+	else
+	{
+		l = l - 1;
+		r = is_palindrome2(s, l);
+		return (r);
+	}
+}
+
 
 
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,3 +13,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
 int _putchar(char c);
+
+int wildcmp(char *s1, char *s2);
+
+int is_palindrome(char *s);
