ee2f153eff8e22a434cff647da42a683d8774bba
AJIyanu <aderemijoseph@live.com>
2022-03-31 08:25:35 +0100

    took me time but i eventually cut corner writing a non rrstricted function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,2 +1,4 @@
 /**
- * is_palindrome - checks if numbers repeat
+ * is_palindrome - checks if numbers repeat from behind
+ * @s: string to compare
+ * Return: 1 on success
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,22 @@
 /**
+ * _sqrt_recursion2 - what will be multiplied together
+ * @n: number to find
+ * @d: to be divided by
+ * Return: -1 if number doesnt exist otherwise sqrt
+ */
+
+
+int _sqrt_recursion2(int n, int d)
+{
+	if (n / d == d && n % d == 0)
+		return d;
+	else if (n == (d / 2) || n < 0)
+		return (-1);
+	else
+		return (_sqrt_recursion2(n, d + 1));
+}
+
+/**
  * _sqrt_recursion - what will be multiplied together
  * @n: number to find
  * Return: -1 if number doesnt exist otherwise sqrt
@@ -7,5 +25,7 @@
 
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-
+	int r;
+	r = _sqrt_recursion2(n, 2);
+	return (r);
+}
