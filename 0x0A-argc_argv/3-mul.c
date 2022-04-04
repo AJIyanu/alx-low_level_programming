1cdff8259c8024fe3cd3ce7ce24b6cb5121bb2df
AJIyanu <aderemijoseph@live.com>
2022-04-04 17:48:51 +0100

    mulllllll

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**                                                              * _atoi - converts string into int value                        * @s: address of the start of the string                        * Return: the string value
+ */                                                             int _atoi(char *s)
+{
+        int sign = 1;
+        int c = 0;
+        int skip = 1;                                                   int i = 0;
+
+        while (s[c] != '\0')                                            {
+                while (s[c] <= '9' && s[c] >= '0')
+                {                                                                       skip = 0;
+                        i = (i * 10) + (s[c] - 48);
+                        c++;                                                    }
+                if (s[c] == '-' && skip == 1)
+                        sign = sign * -1;                                       if (skip == 0)
+                        break;                                                  c++;                                                    }
+        return (sign * i);
+}
+
 
 /**
  * main - prints my name
@@ -22,7 +40,7 @@ int main(int argc, char* argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			num = atoi(argv[i]);
+			num = _atoi(argv[i]);
 			mul = mul * num;
 		}
 		printf("%d\n", mul);
