ae5d955440dd10138655f80a1e6bd3cd919651d0
AJIyanu <aderemijoseph@live.com>
2022-04-04 15:22:59 +0100

    to be continued

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 
 /**
+ * _atoi - converts string into int value
+ * @s: address of the start of the string
+ * Return: the string value
+ */                                                             
+int _atoi(char *s)
+{
+        int sign = 1;
+        int c = 0;
+        int skip = 1;
+        int i = 0;
+
+        while (s[c] != '\0')
+        {
+                while (s[c] <= '9' && s[c] >= '0')
+                {
+                        skip = 0;
+                        i = (i * 10) + (s[c] - 48);
+                        c++;
+                }
+		if (s[c] != '-' && skip == 1)
+			return (-1010);
+
+                if (s[c] == '-' && skip == 1)
+                        sign = sign * -1;                                       if (skip == 0)
+                        break;
+                c++;                                                    }
+        return (sign * i);
+}
+
+
+/**
  * main - prints my name
  * @argc: number
  * @argv: string
@@ -17,12 +48,13 @@ int main(int argc, char* argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!(argv[i] <= '9' && argv[i] >= '0'))
+		num = _atoi(argv[i]);
+		printf("%d\t", num);
+		if (num == )
 		{
 			printf("Error\n");
 			return (1);
 		}
-		num = atoi(argv[i]);
 		sum = sum + num;
 	}
 	printf("%d\n", sum);
