7b88e1fd7cc91b57b9b0be9cb6fc7058283ff807
AJIyanu <aderemijoseph@live.com>
2022-04-06 00:01:56 +0100

    hjj

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,8 +18,6 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 	ptr = (int **)malloc(height * sizeof(int *));
-	if (ptr == NULL)
-		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		*(ptr + i) = (int *)malloc(width * sizeof(int));
