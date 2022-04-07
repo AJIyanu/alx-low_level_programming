798ff19e2f0cfbe5160178eabf4fc7a25477c132
AJIyanu <aderemijoseph@live.com>
2022-04-07 18:48:45 +0100

    gking to test fir betty

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,46 @@
 #include <stdlib.h>
 
 /**
+ * _strcpy - copies a source string to destination
+ * @dest: destination
+ * @src: source
+ *
+ * Return: destination pointer
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * wordlen - counts how many characters in a word
+ * @loc: location of the word to be counted
+ * Return: number of characters counted
+ */
+
+int wordlen(char *loc)
+{
+	int i = 0;
+
+	while (loc[i] != 32 && loc[i] != '\n' && loc[i] != '\t')
+		i++;
+	return (i);
+}
+
+/**
  * countWords - Counts the numbers of words in a string
  * @str: address of the string
  * Return: The numbers of string encountered
@@ -37,40 +77,28 @@ unsigned countWords(char *str)
 
 char **strtow(char *str)
 {
-	int wc;
+	int wc; /* word count */
+	int sc; /* scans through all characters */
+	int wl; /* word length */
+	int arc; /* array counter - move to next memory */
 	char **arr;
 
 	wc = countWords(str);
-	printf("%d\n", wc);
-	arr = (int **)malloc(wc * sizeof(char *));
+	arr = (char **)malloc(wc * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+
+	for (sc = 0; str[sc] != '\0'; sc++)
+	{
+		if (str[sc] != 32 && str[sc] != '\n' && str[sc] != '\t')
+		{
+			wl = wordlen(str + sc);
+			arr[arc] = (char *)malloc(wl * sizeof(char));
+			_strcpy(&arr[arc][0], &str[sc]);
+			arr[arc][wl] = '\0';
+			arc++;
+			sc = sc + wl - 1;
+		}
+	}
 	return (arr);
-}
-
-void print_tab(char **tab)
-{
-    int i;
-
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
-}
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
-int main(void)
-{
-    char **tab;
-
-    tab = strtow("      Holberton School         #cisfun      ");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
 }
