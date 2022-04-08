f5cb032fbccf748e6807f857be6a0df8fc088769
AJIyanu <aderemijoseph@live.com>
2022-04-08 03:19:26 +0100

    do we comply to compile?

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - check the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	return (l);
+}
+
+/**
+ * _strcpy - copies a source string to destination
+ * @dest: destination
+ * @src: source
+ *
+ * Return: destination pointer
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * string_nconcat - combines two strings together. Not all though
+ * @s1: first string
+ * @s2: second string
+ * @n: how many characters from the second string
+ *
+ * Return: pointer to newly allocated memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int sl;
+	int sl2;
+	int msz;
+	int i;
+	char *ptr;
+
+	sl = _strlen(s1);
+	sl2 = _strlen(s2);
+	if (sl2 < n)
+		n = sl2;
+	n++;
+	msz = sl + n;
+	ptr = malloc(msz * sizeof *ptr);
+
+	_strcpy = (ptr, s1);
+	_strcpy = ((ptr + sl - 1), s2);
+	ptr[msz - 1] = '\0';
+
+	return (ptr);
+}
