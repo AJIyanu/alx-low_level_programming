614682fd1a157f5adeebc36b83e31f4b12a95a7a
AJIyanu <aderemijoseph@live.com>
2022-04-12 23:23:11 +0100

    steuxt

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+So now, let us talk about struck
+nn
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - decribes a dog
+ * @age: how old
+ * @name: pet name
+ * @owner: who has it
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
