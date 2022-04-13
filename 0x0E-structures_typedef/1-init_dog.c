cb4c662535110085134ecfbdd3acb7cd94c5ea3f
AJIyanu <aderemijoseph@live.com>
2022-04-13 05:44:18 +0100

    struxr function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - a funtion to initialize dog
+ * @d: struct name
+ * @name: name
+ * @age: how old
+ * @owner: wbo get am
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner){
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
