c984e5491fb468ebc7d7ca891bd566f95e26ecc8
AJIyanu <aderemijoseph@live.com>
2022-04-14 00:22:55 +0100

    just like that?!

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be passed to a function
+ * @f: function that prints
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
