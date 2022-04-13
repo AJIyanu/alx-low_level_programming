57f2fba60c4e2b1a916faa0bed9b4018c4407a23
AJIyanu <aderemijoseph@live.com>
2022-04-13 16:45:15 -0700

    dont think too much

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through array
+ * @array: created array of data
+ * @size: size of the array created
+ * @action: function to pointer
+ *
+ * Return: nothing just acts
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
