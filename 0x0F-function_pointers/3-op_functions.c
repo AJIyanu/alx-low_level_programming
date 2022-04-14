38d7a91f410fe3443ecf95bf84a2facaddfc1b37
AJIyanu <aderemijoseph@live.com>
2022-04-14 03:13:35 +0100

    wont compile

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - addition
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplication
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+		return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+		return (a % b);
+}
