28d3c1efcc2841ff9a260cd604ff2a6f036e9371
AJIyanu <aderemijoseph@live.com>
2022-04-23 03:55:08 +0100

    list freed, time to sleep

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - cycles through the list and frees them
+ * @head: address of the first list
+ *
+ * Return: mo shit is returned
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr = NULL;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		ptr = head;
+		head = head.next;
+		free(ptr);
+	}
+	free(head);
+}
+		
