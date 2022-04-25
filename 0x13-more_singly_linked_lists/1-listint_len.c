d4b5b85849af658ddae6ec58e493733ba3b65bc6
AJIyanu <aderemijoseph@live.com>
2022-04-25 04:54:00 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,22 +3,21 @@
 #include "lists.h"
 
 /**
- * print_listint
+ * listint_len
  * @h: head
  * Return: Number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int node = 0;
 	const listint_t *ptr = h;
 
 	if (ptr == NULL)
-		return (0);
+		return (node);
 
 	while (ptr != NULL)
 	{
-		printf("%d\n", ptr->n);
 		node++;
 		ptr = ptr->next;
 	}
