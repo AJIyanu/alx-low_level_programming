243378a096aaf49c9ebf495e1e77e5740b2165dd
AJIyanu <aderemijoseph@live.com>
2022-04-30 04:36:55 +0100

    exor

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * flip_bits - what has changed
+ * @n: num 1
+ * @m: num2
+ *
+ * Return: number of changed bit
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int l = 0;
+	int i = 0;
+
+	l = m ^ n;
+
+	while (l > 1)
+	{
+		l = l >> 1;
+		if ((l & 1) == 1)
+			i++;
+	}
+	return (i + 1);
+}
