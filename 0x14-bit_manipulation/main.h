0bd42c637c82219ecf8706501be59f1787bb978d
AJIyanu <aderemijoseph@live.com>
2022-04-30 04:53:04 +0100

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -15,4 +15,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 void _putchar(char c);
+
+int get_endianness(void);
 #endif
