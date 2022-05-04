2bf1db6b3d7c5b4ff045668554bc58f04b8b2240
AJIyanu <aderemijoseph@live.com>
2022-05-04 12:13:23 +0100

    reset counter

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *ptr = head;
 	unsigned int n = 0;
 
-	while (n <= index)
+	while (n < index)
 	{
 		if (ptr == NULL)
 			return (NULL);
