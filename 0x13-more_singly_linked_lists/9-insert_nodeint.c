31aec7ba7995b4ccc99507f1cfc3f92a5ba82dc9
AJIyanu <aderemijoseph@live.com>
2022-05-04 12:53:43 +0100

    index 0 and null taken to consideration

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,13 +23,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-
-	if (temp == NULL)
-		return (NULL);
-
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
 		return (NULL);
+	if (temp == NULL)
+	{
+		ptr->n = n;
+		ptr->next = NULL;
+		*head = ptr;
+	}
 	if (idx == 0)
 	{
 		ptr->n = n;
