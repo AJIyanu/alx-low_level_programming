73549fd11828b62a55f8106e997efa65e0be3d38
AJIyanu <aderemijoseph@live.com>
2022-05-04 11:33:19 +0100

    gog gog go

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,9 +6,14 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure 
+ * Description: singly linked list node structure
+ * 
  */
-
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 typedef struct listint_s
 {
 	int n;
