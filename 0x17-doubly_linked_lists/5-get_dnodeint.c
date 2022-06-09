59f1e3409a9ca6830c0d7857e770430638cedb89
AJIyanu <aderemijoseph@live.com>
2022-06-09 22:10:47 +0100

    break

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - address of node at index
+ * @head: header
+ * @index: POS sorry Point of Return
+ *
+ * Return: address of index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+	unsigned int count = 0;
+
+	if (ptr == NULL)
+		return(NULL);
+	while(ptr != NULL)
+	{
+		if (count == index)
+			return(ptr);
+		count++;
+		ptr = ptr->next;
+	}
+	return(ptr);
+}
