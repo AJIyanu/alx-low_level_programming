4d08bba61675139d2c63ef64557d89505bc8771a
AJIyanu <aderemijoseph@live.com>
2022-06-10 21:42:35 +0100

    out of range

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -36,7 +36,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		ptr = ptr->next;
 		count++;
 	}
-	if (ptr->next == NULL)
+	if (ptr->next == NULL && count == index)
 	{
 		if (index == 0)
 		{
