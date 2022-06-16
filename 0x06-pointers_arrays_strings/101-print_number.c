74d8e7c2f2e5d8fc9db9b94d0d57610ac3ff402c
AJIyanu <aderemijoseph@live.com>
2022-06-16 09:26:22 +0100

    compiled

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,9 +11,20 @@
 
 void print_number(int n)
 {
+	int mod;
+
+	if (n < 0)
+	{
+		putchar('-');
+		n *= -1;
+	}
+	mod = n % 10;
+
 	if (n < 10)
 		putchar(n + 48);
-	print_number(n / 10);
-	putchar(n % 10 + 48
-
+	else
+	{
+		print_number(n / 10);
+		putchar(mod + 48);
+	}
 }
