7ff2cff1368953e538d3c8aa456d13d8cdd3d5da
AJIyanu <aderemijoseph@live.com>
2022-06-16 09:02:03 +0100

    recursion

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_number - print number in char
+ * @n: Number to be printed
+ *
+ * Return: no shit
+ */
+
+void print_number(int n)
+{
+	int mod;
+
+	if (n < 10)
+		putchar(n + 48);
+	else
+		print_number(n % 10);
+}
