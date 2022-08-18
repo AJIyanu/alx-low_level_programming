904be54a0760eb2eb6d699e494b71b8e559d0ecf
AJIyanu <aderemijoseph@live.com>
2022-08-18 09:49:09 +0100

    now?

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,9 @@ This module is used to calculate the perimeter of island
 """
 
 
+import re
+
+
 def island_perimeter(grid):
     """
     This function does the calculation
@@ -15,6 +18,25 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(col):
             if grid[i][j] == 1:
-                result = result + 1
+                try:
+                    if grid[i - 1][j] == 0:
+                        result = result + 1
+                except IndexError:
+                    result = result + 1
+                try:
+                    if grid[i + 1][j] == 0:
+                        result = result + 1
+                except IndexError:
+                    result = result + 1
+                try:
+                    if grid[i][j - 1] == 0:
+                        result = result + 1
+                except IndexError:
+                    result = result + 1
+                try:
+                    if grid[i][j + 1] == 0:
+                        result = result + 1
+                except IndexError:
+                    result = result + 1
 
     return (result)
