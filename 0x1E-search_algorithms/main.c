c2cdf6ec335701a68294f04cac8ba0e78e996295
AJIyanu <aderemijoseph@live.com>
2022-10-05 14:30:34 +0100

    I cannot have two mains

--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -17,16 +17,7 @@ int main(void)
     printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
     printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
-}
 
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
     int array[] = {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     };
