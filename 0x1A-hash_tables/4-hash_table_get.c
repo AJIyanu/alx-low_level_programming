3d4e4199dcb99d8cf3d8bd98b33900f422cb9f4e
AJIyanu <aderemijoseph@live.com>
2022-12-19 09:46:51 +0100

    retrieve value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve value
+ * @ht: table address id
+ * @key: key to retrieve
+ *
+ * Return: address of value or Null
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (ht == NULL || key == NULL || *key = "\0")
+		return (NULL);
+	index = key_index(key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	return ((ht->array[index])->value);
+}
+	
