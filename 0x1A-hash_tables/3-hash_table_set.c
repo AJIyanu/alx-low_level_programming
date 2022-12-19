b6f5f5751e2ba84d4e13e5138c11b835e9ce3cfe
AJIyanu <aderemijoseph@live.com>
2022-12-19 04:45:08 +0100

    cast type

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(node->key, key);
 	node->value = malloc(strlen(value) + 1);
 	strcpy(node->value, value);
-	index = key_index(node->key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		ht->array[index] = node;
 	else
