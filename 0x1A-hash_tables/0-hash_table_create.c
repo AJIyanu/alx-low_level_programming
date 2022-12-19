36d8c9711ad45117f48642587171e14ccc8d151b
AJIyanu <aderemijoseph@live.com>
2022-12-19 02:00:42 +0100

    more memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 
 /**
  * hash_table_create - creates table and initialize to null
- * size: size of the table needed
+ * @size: size of the table needed
  *
  * Return: pointer to table
  */
@@ -11,12 +11,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 	return (table);
 }
