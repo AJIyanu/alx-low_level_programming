b17e63ea2e981013812edc8a9c415e2b11fb61dd
AJIyanu <aderemijoseph@live.com>
2022-12-19 10:10:32 +0100

    fix collision bugs

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,12 +12,15 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
+	hash_node_t *node = NULL;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
-	return ((ht->array[index])->value);
+	node = ht->array[index];
+	while (node->next != NULL)
+		node = node->next;
+	return (node->value);
 }
-	
