09c2d4b2aa8ffbaa6d42d5e0191ba2c4c70957a4
AJIyanu <aderemijoseph@live.com>
2022-12-19 03:20:04 +0100

    hash function

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function based on djb2 algorith
+ * @str: the sring
+ *
+ * Return: key value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hval = 5381;
+	int s = 0;
+
+	while(*str)
+	{
+		s = *str;
+		hval = (hval << 5) + hval + s;
+		str++;
+	}
+	return (hval);
+}
+
